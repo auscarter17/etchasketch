0f177662841509336ac4ae39103a0c40dd02fbc0
auscarter17 <auscarter17@gmail.com>
2023-03-23 14:35:24 -0400

    mouseover in grid working

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let container = document.getElementById('container');
+let containerEl = document.getElementById('container');
 
+let gridCount = 256;
+
+for (i = 0; i < gridCount; i++) {
+  const squares = document.createElement('div');
+  squares.className = 'gridSquare';
+  containerEl.appendChild(squares);
+  console.log('one squary boi');
+}
+
+let squares = document.querySelectorAll('.gridSquare');
+
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    console.log('drawn');
+    square.classList.add('drawn');
+  })
+})
